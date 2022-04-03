45580ad5f51e97291dbf5004bff3564a3924d1ae
Thanh Nguyen <thanhnguyenduy2304@gmail.com>
2022-04-03 12:32:14 +0700

    Add puts updated metadata

--- app/sidekiq/update_youtube_videos_metadata_job.rb
+++ app/sidekiq/update_youtube_videos_metadata_job.rb
@@ -4,17 +4,18 @@ class UpdateYoutubeVideosMetadataJob
   UPDATE_PERIOD_THRESHOLD_IN_HOURS = ENV["VIDEO_UPDATE_PERIOD_THRESHOLD_IN_HOURS"] || 24
 
   def perform(document_ids)
+    puts 'UpdateYoutubeVideosMetadataJob'
     if document_ids.kind_of?(Array) && !document_ids.empty?
       documents = Document.id_list(document_ids).includes(:upload_media)
       documents.each do |document|
         youtube_video = document.upload_media.first
         if (Time.now - youtube_video.updated_at) / 86400 > UPDATE_PERIOD_THRESHOLD_IN_HOURS
+          puts "Update #{youtube_video.id} metadata"
           video_metadata = GetYoutubeVideoInfo.call(youtube_video.url)
 
           youtube_video.update({
                                  meta_data: video_metadata
                                })
-          puts 'aaaaaa'
         end
       end
     end
